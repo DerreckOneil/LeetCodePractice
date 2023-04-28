b9fca73476da7222e830a3d602dd6eb97ccae1ef
Derreck Oneil <derreckoneil@gmail.com>
2023-04-28 01:20:35 -0400

    Initial commit

---
+++ LeetCodeProj/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+public class Program
+{
+    
+    public static void Main(string[]args)
+    {
+        Console.WriteLine("Hello world");
+        Console.WriteLine("PalindromeSeq: " + LongestPalindromeSubseq("aaaaaaaaa"));
+    }
+
+    public static int LongestPalindromeSubseq(string s)
+    {
+        int palindromeSubseq = 1;
+
+        Dictionary<char, int> map = new Dictionary<char, int>();
+        char[] chars = s.ToCharArray();
+        foreach(char i in chars)
+        {
+            int occurance = 0;
+            Console.WriteLine(i);// gives ya 'a'
+            foreach(char j in chars)
+            {
+
+                if (i == j)
+                {
+                    occurance++;
+                    //map.Add(i, occurance);
+                }
+            }
+            map.Add(i, occurance);
+            Console.WriteLine("Character: " + i + "Occurance: " + occurance);
+        }
+        
+        
+
+        return palindromeSubseq;
+    }
+}
