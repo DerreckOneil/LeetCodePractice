5edb43a1d877b417af028f9b3cac84ccac5fcfc2
Derreck Oneil <derreckoneil@gmail.com>
2023-08-29 00:59:38 -0400

    LeetCode double queue to stack problem completed successfully.

---
+++ ImplementStackUsingQueues/MainProgram.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+public class MainProgram
+{
+
+    public static void Main(string[] args)
+    {
+        MyStack obj = new MyStack();
+        obj.Push(1);
+        obj.Push(2);
+        obj.Push(3);
+
+        Console.WriteLine(obj.ToString());
+        int param_2 = obj.Pop();
+        int param_3 = obj.Top();
+        bool param_4 = obj.Empty();
+
+        Console.WriteLine("Pop: " + param_2 
+                         +"\nTop: " + param_3
+                         +"\nEmpty: " + param_4);
+
+        /*
+        */
+    }
+
+}
---
+++ ImplementStackUsingQueues/MyStack.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+
+
+public class MyStack
+{
+    private Queue<int> queue1;
+    private Queue<int> queue2;
+
+    public Queue<int> Queue1 => queue1;
+    public Queue<int> Queue2 => queue2;
+
+    public MyStack()
+    {
+        queue1 = new Queue<int>();
+        queue2 = new Queue<int>();
+    }
+
+    public void Push(int x)
+    {
+        queue1.Enqueue(x);
+        queue2 = queue1;
+    }
+
+    public int Pop()
+    {
+        int num = queue1.Reverse().First();
+        Console.WriteLine("Need to Dequeue this " + num);
+        
+        List<int> ints = new List<int>();
+        ints = queue1.ToList();
+        ints.RemoveAt(queue1.Count - 1); //Remove the last 
+        queue1.Clear();
+
+        for(int i =0; i < ints.Count; i++)
+        {
+            queue1.Enqueue((int)ints[i]);
+        }
+        
+
+        return num;
+    }
+
+    public int Top()
+    {
+        int num;
+        num = queue2 != null ? queue2.Reverse().First() : 0; //Can't call double returns...only return a local variable.
+        return num;
+    }
+
+    public bool Empty()
+    {
+        bool isEmpty = queue1.Count == 0;
+        return isEmpty;
+    }
+
+    public override string ToString() => String.Join(',',queue2.Reverse());
+    
+}
+
